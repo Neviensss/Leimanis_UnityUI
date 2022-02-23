c0add9afd03b6f66597aa5d5e106de5ac5d1bb1a
leimanis <niks.leimanis3@gmail.com>
2022-02-23 12:11:24 +0200

    Pievientas funkcijas izveleties telus un tiem iedot vardu

---
+++ Assets/Skripti/CharHolder.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharHolder : MonoBehaviour {
+	public GameObject attelaTuretajs;
+	public Sprite[] attelumasivs;
+
+	public void izkritosais(int skaitlis){
+		if (skaitlis == 0)
+			attelaTuretajs.GetComponent<Image> ().sprite = attelumasivs [0];
+		else if (skaitlis == 1)
+			attelaTuretajs.GetComponent<Image> ().sprite = attelumasivs [1];
+	}
+}
---
+++ Assets/Skripti/CharInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharInfo : MonoBehaviour {
+	public string teksts;
+	public int vecums = 0;
+	public GameObject ievadesLauks;
+	public GameObject vecumsLauks;
+	public GameObject TekstaAttelosanasLauks;
+
+	public void darbibasArTekstu(){
+		teksts = ievadesLauks.GetComponent<Text> ().text;
+		vecums = int.Parse(vecumsLauks.GetComponent<Text> ().text);
+			TekstaAttelosanasLauks.GetComponent<Text> ().text = "Tēls " + teksts.ToUpper () + " ir " + vecums + " gadus vecs!";
+	}
+}
