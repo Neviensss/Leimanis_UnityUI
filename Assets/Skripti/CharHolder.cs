ed25902f4be58f7d39c90f159f73f81584a4e635
leimanis <niks.leimanis3@gmail.com>
2022-02-23 14:42:22 +0200

    Pievienota drebju maina, kustinasana, tela lieluma maina

--- Assets/Skripti/CharHolder.cs
+++ Assets/Skripti/CharHolder.cs
@@ -4,13 +4,42 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class CharHolder : MonoBehaviour {
+	public GameObject kleita;
+	public GameObject kleitaReal;
+	public GameObject svarkiReal;
+	public GameObject svarki;
+	public GameObject slaideris;
+	public GameObject slaiderisPlatums;
 	public GameObject attelaTuretajs;
 	public Sprite[] attelumasivs;
 
+	void Start(){
+		kleita.SetActive (false);
+		svarki.SetActive (false);
+	}
+
 	public void izkritosais(int skaitlis){
-		if (skaitlis == 0)
+		if (skaitlis == 0){
 			attelaTuretajs.GetComponent<Image> ().sprite = attelumasivs [0];
-		else if (skaitlis == 1)
+		kleita.SetActive (false);
+		svarki.SetActive (false);
+			kleitaReal.SetActive (false);
+			svarkiReal.SetActive (false);
+	}
+		else if (skaitlis == 1){
 			attelaTuretajs.GetComponent<Image> ().sprite = attelumasivs [1];
+			kleita.SetActive (true);
+			svarki.SetActive(true);
+	}
+}
+	public void mainitGarumu(){
+		float izmers = slaideris.GetComponent<Slider> ().value;
+		float izmersPlatums = slaiderisPlatums.GetComponent<Slider> ().value;
+		attelaTuretajs.transform.localScale = new Vector2 (1f*izmers, 1f*izmersPlatums);
+	}
+	public void mainitPlatumu(){
+		float izmers = slaiderisPlatums.GetComponent<Slider> ().value;
+		float izmersGarums = slaideris.GetComponent<Slider> ().value;
+		attelaTuretajs.transform.localScale = new Vector2 (1f*izmersGarums, 1f*izmers);
 	}
 }
---
+++ Assets/Skripti/DrebjuKustinasana.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DrebjuKustinasana : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+	private RectTransform transformacijasLogs;
+	public Canvas kanva;
+
+	void Start () {
+		transformacijasLogs = GetComponent<RectTransform>();
+	}
+
+	public void OnPointerDown(PointerEventData notikums){
+		Debug.Log ("Kreisais Klikśķis uz objektu");
+	}
+
+	public void OnBeginDrag(PointerEventData notikums){
+		Debug.Log ("uzskatsa objekta vilkśana");
+	}
+
+	public void OnDrag(PointerEventData notikums){
+		transformacijasLogs.anchoredPosition += notikums.delta / kanva.scaleFactor;
+	}
+
+	public void OnEndDrag(PointerEventData notikums){
+		Debug.Log (" objekta vilkśana partraukta");
+	}
+}
